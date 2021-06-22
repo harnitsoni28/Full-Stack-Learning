b265c63af605d2b3a325744f15e04500c36ad5a3
harnitsoni28 <harnitsoni8@gmail.com>
2021-06-22 21:24:23 +0530

    update

---
+++ js/demo.js
@@ -0,0 +1,44 @@
+// alert("Welcome to JavaScript !")
+// prompt("kya haal hai bhai ke ? ")
+// console.log("lo aa gaye ham console mai !")
+
+// var age = prompt("Enter age")
+// var multiply = age * 50
+// alert ("age" +  multiply)
+
+// var a=2
+// var b=5
+// if (a>b){
+//     alert("bhai a bada hai")
+// }
+//     else{
+//         alert("bhai b bada hai")
+//     }
+
+// var x=0
+
+// while(x<5){
+//     console.log("currently x is : " + x)
+//     x++;
+// }
+
+// for(var x=0; x<5; x++){
+//      console.log("current x value" + x)
+// }
+
+// function hello(){
+//     alert("hello world!")
+// }
+
+// hello() // call function
+
+// function sum(a,b){
+//     return(a+b)
+// }
+
+// var c=(2+3)
+// alert("sum is : "+ c) 
+
+var p=document.querySelector("p")
+
+p.style.color="red"
